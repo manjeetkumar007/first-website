6e1d913e49dd826bac4cbebaa3e5754ade94aefc
Manjeet <manjeetkkumar77@gmail.com>
2026-01-03 18:43:38 -0500

    structured files

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+
+class HelloWorld {
+
+     static void printing(){
+       System.out.println("Hello World");
+     }
+    
+
+    
+
+public static void main(String[] args){
+        HelloWorld.printing();
+    }
+    }
